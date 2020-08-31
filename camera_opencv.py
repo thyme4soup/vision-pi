3db3aef0d8889bab4d8b137f7639a078acec346f
Michael Chenevey <mchenevey7@gmail.com>
2020-08-31 10:54:10 -0700

    missed some

--- camera_opencv.py
+++ camera_opencv.py
@@ -51,27 +51,27 @@ class Camera(BaseCamera):
             self.net.setInput(blob)
             detections = self.net.forward()
             # loop over the detections
-        	for i in np.arange(0, detections.shape[2]):
-        		# extract the confidence (i.e., probability) associated with
-        		# the prediction
-        		confidence = detections[0, 0, i, 2]
-        		# filter out weak detections by ensuring the `confidence` is
-        		# greater than the minimum confidence
-        		if confidence > args["confidence"]:
-        			# extract the index of the class label from the
-        			# `detections`, then compute the (x, y)-coordinates of
-        			# the bounding box for the object
-        			idx = int(detections[0, 0, i, 1])
-        			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        			(startX, startY, endX, endY) = box.astype("int")
-        			# draw the prediction on the frame
-        			label = "{}: {:.2f}%".format(CLASSES[idx],
-        				confidence * 100)
-        			cv2.rectangle(img, (startX, startY), (endX, endY),
-        				COLORS[idx], 2)
-        			y = startY - 15 if startY - 15 > 15 else startY + 15
-        			cv2.putText(img, label, (startX, y),
-        				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+            for i in np.arange(0, detections.shape[2]):
+                # extract the confidence (i.e., probability) associated with
+                # the prediction
+                confidence = detections[0, 0, i, 2]
+                # filter out weak detections by ensuring the `confidence` is
+                # greater than the minimum confidence
+                if confidence > args["confidence"]:
+                    # extract the index of the class label from the
+                    # `detections`, then compute the (x, y)-coordinates of
+                    # the bounding box for the object
+                    idx = int(detections[0, 0, i, 1])
+                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                    (startX, startY, endX, endY) = box.astype("int")
+                    # draw the prediction on the frame
+                    label = "{}: {:.2f}%".format(CLASSES[idx],
+                        confidence * 100)
+                    cv2.rectangle(img, (startX, startY), (endX, endY),
+                        COLORS[idx], 2)
+                    y = startY - 15 if startY - 15 > 15 else startY + 15
+                    cv2.putText(img, label, (startX, y),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
             # encode as a jpeg image and return it
             yield cv2.imencode('.jpg', img)[1].tobytes()

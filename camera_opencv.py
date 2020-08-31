3e78b96e5e6476fba987fef9c937bcd407039784
Michael Chenevey <mchenevey7@gmail.com>
2020-08-31 10:49:18 -0700

    added model and cv computer vision code

--- camera_opencv.py
+++ camera_opencv.py
@@ -1,12 +1,30 @@
 import os
 import cv2
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
 from base_camera import BaseCamera
 
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+MODEL_PATH = "models/"
+MODEL_NAME = "MobileNetSSD_deploy"
 
 class Camera(BaseCamera):
     video_source = 0
 
     def __init__(self):
+        # load our serialized model from disk
+        print("[INFO] loading model...")
+        self.net = cv2.dnn.readNetFromCaffe(MODEL_PATH + MODEL_NAME + ".prototxt",
+                                            MODEL_PATH + MODEL_NAME + ".caffemodel")
+
         if os.environ.get('OPENCV_CAMERA_SOURCE'):
             Camera.set_video_source(int(os.environ['OPENCV_CAMERA_SOURCE']))
         super(Camera, self).__init__()
@@ -24,6 +42,36 @@ class Camera(BaseCamera):
         while True:
             # read current frame
             _, img = camera.read()
+            img = imutils.resize(img, width=400)
+            (h, w) = img.shape[:2]
+            blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+                                         0.007843,
+                                         (300, 300),
+                                         127.5)
+            self.net.setInput(blob)
+	        detections = self.net.forward()
+            # loop over the detections
+        	for i in np.arange(0, detections.shape[2]):
+        		# extract the confidence (i.e., probability) associated with
+        		# the prediction
+        		confidence = detections[0, 0, i, 2]
+        		# filter out weak detections by ensuring the `confidence` is
+        		# greater than the minimum confidence
+        		if confidence > args["confidence"]:
+        			# extract the index of the class label from the
+        			# `detections`, then compute the (x, y)-coordinates of
+        			# the bounding box for the object
+        			idx = int(detections[0, 0, i, 1])
+        			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+        			(startX, startY, endX, endY) = box.astype("int")
+        			# draw the prediction on the frame
+        			label = "{}: {:.2f}%".format(CLASSES[idx],
+        				confidence * 100)
+        			cv2.rectangle(img, (startX, startY), (endX, endY),
+        				COLORS[idx], 2)
+        			y = startY - 15 if startY - 15 > 15 else startY + 15
+        			cv2.putText(img, label, (startX, y),
+        				cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
             # encode as a jpeg image and return it
             yield cv2.imencode('.jpg', img)[1].tobytes()
